21dbd0873bb27c66e6c1f26012dacd7449ca5879
Jesse Falconer <jesse.falconer@spark.re>
2019-12-01 23:48:24 -0800

    Solution to 1b

---
+++ advent1b.m
@@ -0,0 +1,17 @@
+format longG;
+masses = []; 
+sum = 0;
+for i = 1 : length(masses)
+    sum = sum + recursion(masses(i))
+end
+
+display(additive_fuel);
+
+function out = recursion(mass)
+    fuel = floor((mass / 3) - 2)
+    if fuel >= 0
+        out = fuel + recursion(fuel);
+    else
+        out = 0;
+    end
+end
